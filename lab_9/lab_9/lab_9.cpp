260ff289f082872d3720001f912109d34b0e8c46
kostyl2220 <kostyl2220@ukr.net>
2016-05-10 21:52:32 +0300

    I think, I wrote toooooo many comments in code. Good luck. Somewere there is your task

--- lab_9/lab_9/lab_9.cpp
+++ lab_9/lab_9/lab_9.cpp
@@ -8,125 +8,172 @@
 
 const int MAX_LEN = 50;
 
-struct Furniture {
-	char name[MAX_LEN];
-	char typeMain[MAX_LEN];
-	char typeAdd[MAX_LEN];
-	char matherial[MAX_LEN];
-	char color[MAX_LEN];
-	int price;
-	int countOfProduct;
+struct sizes{
+	int height;
+	int lenght;
+	int width;
+};
+
+struct Furniture { //terrble homunkyl structure with union that consists structure in structure. 
+	char name[MAX_LEN];  //name of item
+	char typeMain[MAX_LEN]; //main type table or Chair (It's for choosing union fields)
+	char typeAdd[MAX_LEN];  //additional field (kitchen or work or bar item)
+	char matherial[MAX_LEN];	//matherial that was used making it. Be sure that your table wasn't made in China with using unnatural elements.
+	char color[MAX_LEN]; //color of your item. Let's work without pink colors, ok?
+	int price;  //it's price of item
+	int amountOfProduct;  //count (amount) of product that is on stock
 	union {
-		struct {
-			char *typeBack;
-			char *typeSeat;
-			char *colorUpholstery;
+		struct {  //structure for chair
+			char typeBack[MAX_LEN];  //type of back of chair
+			char typeSeat[MAX_LEN];  //type of seat of chair
+			char colorUpholstery[MAX_LEN];  // color of that... I am tired to write. If third guy of our team read's it. Come to me, I'll give you a candle. Ginger princess, you is the best <3 :*
 		} chair;
-		struct
+		struct //structure for table
 		{
-			char *typeTable;
-			int size;
+			char typeTable[MAX_LEN];  //type of table (I used direct in my task)
+			sizes size;  //sizes described before homunkyl
 		} table;
 	} spec;
 };
 
-struct List{
+struct List{  //we can delete it, but I can't kill my child
 	Furniture data;
 	List *next;
 };
 
 const int COUNT = 10;
 
-int fillCollection(Furniture *head){
-	strcpy(head[0].name,"Victoria");
+int fillCollection(Furniture *head){ //method for filling collection, my own examples if you, Nastya, cause I sure that Valentin won't read it,
+									//want to scange some names, you are welcome, but don't touch one table, we must have it for my task.
+									//add new items for your one
+
 	strcpy(head[0].typeMain, "Chair");
-	strcpy(head[0].typeAdd, " bar");
+	strcpy(head[0].typeAdd, "bar");
 	strcpy(head[0].matherial, "Wood");
 	strcpy(head[0].color, "Black");
 	head[0].price = 500;
-	head[0].countOfProduct = 16;
-	head[0].spec.chair.colorUpholstery = "Red";
-	head[0].spec.chair.typeBack = "Huge";
-	head[0].spec.chair.typeSeat = "Huge";
+	head[0].amountOfProduct = 16;
+	strcpy(head[0].spec.chair.colorUpholstery, "Red"); //F
+	strcpy(head[0].spec.chair.typeBack, "Huge");
+	strcpy(head[0].spec.chair.typeSeat, "Huge");
 
-	strcpy(head[1].name, "Conqueror");
+	strcpy(head[1].name, "Conqueror");//I
 	strcpy(head[1].typeMain, "Table");
-	strcpy(head[1].typeAdd, " computer");
+	strcpy(head[1].typeAdd, "computer");
 	strcpy(head[1].matherial, "Wood");
-	strcpy(head[1].color, "Drak brown");
+	strcpy(head[1].color, "Dark brown");
 	head[1].price = 2200;
-	head[1].countOfProduct = 7;
-	head[1].spec.table.size = 600;
-	head[1].spec.table.typeTable = "Rectangle";
+	head[1].amountOfProduct = 7;
+	head[1].spec.table.size.height = 88;//L
+	head[1].spec.table.size.width = 70;
+	head[1].spec.table.size.lenght = 170;
+	strcpy(head[1].spec.table.typeTable, "Direct");
 
 	strcpy(head[2].name, "Casual");
-	strcpy(head[2].typeMain, "Chair");
-	strcpy(head[2].typeAdd, " kitchen");
+	strcpy(head[2].typeMain, "Chair");//L
+	strcpy(head[2].typeAdd, "kitchen");
 	strcpy(head[2].matherial, "Steel");
 	strcpy(head[2].color, "Gray");
 	head[2].price = 160;
-	head[2].countOfProduct = 38;
-	head[2].spec.chair.colorUpholstery = "None";
-	head[2].spec.chair.typeBack = "Little";
-	head[2].spec.chair.typeSeat = "Usual";
+	head[2].amountOfProduct = 38;
+	strcpy(head[2].spec.chair.colorUpholstery, "None");
+	strcpy(head[2].spec.chair.typeBack, "Little");
+	strcpy(head[2].spec.chair.typeSeat, "Usual");//I
 
 	strcpy(head[3].name, "Softness");
 	strcpy(head[3].typeMain, "Chair");
-	strcpy(head[3].typeAdd, " work");
-	strcpy(head[3].matherial, "Plastic and scin");
+	strcpy(head[3].typeAdd, "work");
+	strcpy(head[3].matherial, "Plastic and skin");//N
 	strcpy(head[3].color, "Black");
 	head[3].price = 780;
-	head[3].countOfProduct = 3;
-	head[3].spec.chair.colorUpholstery = "Brown";
-	head[3].spec.chair.typeBack = "Soft";
-	head[3].spec.chair.typeSeat = "Soft";
+	head[3].amountOfProduct = 3;
+	strcpy(head[3].spec.chair.colorUpholstery, "Brown");
+	strcpy(head[3].spec.chair.typeBack, "Soft");
+	strcpy(head[3].spec.chair.typeSeat, "Soft");
 
-	return 4;
+	return 4;//G
 }
 
-void writeToBinaryFile(const char *path, Furniture *furn, int count) {
-	FILE *f = fopen(path, "wb");
-	fwrite(furn, sizeof(Furniture), count, f);
-	fclose(f);
-}
+void writeToBinaryFile(Furniture *furn, int count) {  //method to write data to new BINARY (Wooooow!!!! Fantastic!!1) file
+	printf("Enter the name of file: ");  // four 
+	char path[MAX_LEN];					//lines
+	gets(path);							//to enter
+	strcat(path, ".dat");				//the name
 
-int ReadFromBinaryFile(const char *path, Furniture *furn) {
-	FILE *f = fopen(path, "rb");
+	FILE *f = fopen(path, "wb");	//opening
+	fwrite(furn, sizeof(Furniture), count, f);  //writing
+	fclose(f); //closing
+}  //fantastic, right?
+
+int ReadFromBinaryFile(Furniture *furn) {  //for reading
+	printf("Enter the name of file: ");  // four 
+	char path[MAX_LEN];					//lines
+	gets(path);							//to enter
+	strcat(path, ".dat");				//the name
+
+	FILE *f = fopen(path, "rb");  //opening
 	int i = 0;
-	while (!feof(f)){
-		fread(furn + i++, sizeof(Furniture), 1, f);
+	while (!feof(f)){  //while not end
+		fread(furn + i++, sizeof(Furniture), 1, f);  //read
 	}
-	fclose(f);
-	return i - 1;
+	fclose(f); //close
+	return i - 1;  //return the size with little feature. Dont ask. It works
 }
 
-void outCollection(Furniture *furn, int count) {
+void outCollection(Furniture *furn, int count) { // out collection
 	for (int i = 0; i < count; ++i) {
-		printf("Name: %s\nType: %s%s\nMatherial: %s\nColor: %s\nPrice: %d\nCount of product: %d\n\n",
-			furn[i].name, furn[i].typeMain, furn[i].typeAdd, furn[i].matherial, furn[i].color, furn[i].price, furn[i].countOfProduct);
+		printf("Name: %s\nType: %s %s\nMatherial: %s\nColor: %s\nPrice: %d\nCount of product: %d\n\n",
+			furn[i].name, furn[i].typeMain, furn[i].typeAdd, furn[i].matherial, furn[i].color, furn[i].price, furn[i].amountOfProduct); //for both items the same
 		if (strcmp(furn[i].typeMain, "Chair") == 0) {
-			printf("Type of back: %s\nType of seat: %s\nColor of Upholstery: %s\n__________________________________________________\n", 
+			printf("Type of back: %s\nType of seat: %s\nColor of Upholstery: %s\n__________________________________________________\n",   //if item is a CHAIR
 				furn[i].spec.chair.typeBack, furn[i].spec.chair.typeSeat, furn[i].spec.chair.colorUpholstery);
 		}
 		if (strcmp(furn[i].typeMain, "Table") == 0) {
-			printf("Type of table: %s\nSize of table: %d\n__________________________________________________\n",
-				furn[i].spec.table.typeTable, furn[i].spec.table.size);
+			printf("Type of table: %s\nSize of table:\n\tHeight: %d\n\tWidth: %d\n\tLenght: %d\n__________________________________________________\n", //if item is a TABLE
+				furn[i].spec.table.typeTable, furn[i].spec.table.size.height, furn[i].spec.table.size.width, furn[i].spec.table.size.lenght);
 		}
 	}
 }
 
-void mainMenu(Furniture *furn){
+void thirdTask(Furniture *furn, int count){ //My task, I dont want to discuss it
+	system("cls");
+	printf("Third task.\n");  // or want
+	printf("Enter the name of new file: ");
+	char path[MAX_LEN]; //hihi
+	gets(path);
+	strcat(path, ".dat");  //enter the name of file
+
+	outCollection(furn, count);  //out collection to choose proper size of table
+	printf("Enter the size of needing tables (height, width, lenght):\n"); //choose proper size of table
+	sizes size;
+	scanf("%d%d%d", &size.height, &size.width, &size.lenght);
+
+	FILE *f = fopen(path, "wb"); //opening
+	for (int i = 0; i < count; ++i) {
+		if ((strcmp(furn[i].typeMain, "Table") == 0) && //HUUUUUUUUUUUU
+			(strcmp(furn[i].spec.table.typeTable, "Direct") == 0) && //GEEEEEEEEE
+			(strcmp(furn[i].typeAdd, "computer") == 0) &&
+			furn[i].spec.table.size.height == size.height &&  //Condition
+			furn[i].spec.table.size.width == size.width &&
+			furn[i].spec.table.size.lenght == size.lenght) { //dont read it
+			fwrite(furn + i, sizeof(Furniture), 1, f); //write one element to file
+		}
+	}
+	fclose(f); //close file
+}
+
+void mainMenu(Furniture *furn){ //menu with points
 	int count = 0;
 	while (true) {
 		printf("Menu:\n");
 		printf("\t1.Fill collection\n");
 		printf("\t2.Print collection\n");
-		printf("\t3.Add element to collection\n");
-		printf("\t4.Delete element of collection\n");
-		printf("\t5.Editing element of collection\n");
-		printf("\t6.First task\n");
-		printf("\t7.Second task\n");
-		printf("\t8.Third task\n");
+		printf("\t3.Add element to collection\n"); //TODO...
+		printf("\t4.Delete element of collection\n");//TODO...
+		printf("\t5.Editing element of collection\n");//TODO-
+		printf("\t6.First task\n");//TODO-
+		printf("\t7.Second task\n");//TODO
+		printf("\t8.Third task\n");  //TODODODOOOOOOO... TODODODO (it's song) you should do 5 tasks above
 		printf("\t9.Read from file\n");
 		printf("\t0.Write to file\n");
 		printf("\tq.Exit\n");
@@ -135,30 +182,33 @@ void mainMenu(Furniture *furn){
 		switch (ch)
 		{
 		case '1':
-			count = fillCollection(furn);
+			count = fillCollection(furn);  //and add your cases there
 			break;
 		case '2':
-			outCollection(furn, count);
+			outCollection(furn, count); //with your methods
 			break;
 		case '9':
-			count = ReadFromBinaryFile("input.dat", furn);
+			count = ReadFromBinaryFile(furn);//and organise prototypes (Forgot this word)
 			break;
 		case '0':
-			writeToBinaryFile("input.dat", furn, count);
+			writeToBinaryFile(furn, count);//pleeeease
+			break;
+		case '8':
+			thirdTask(furn, count); //Good like
 			break;
 		case 'q':
-			return;
+			return; //You is the best, my dove <3
 		default:
 			return;
 		}
 	}
 }
 
-int _tmain(int argc, _TCHAR* argv[])
+int _tmain(int argc, _TCHAR* argv[])  //short main method
 {
-	Furniture furn[COUNT];
+	Furniture furn[COUNT]; //Lets leave all jokes about "short" :(
 	mainMenu(furn);
-	system("pause");
-	return 0;
+	system("pause");//and my English
+	return 0;//I hope, nobody died while I was writing it.
 }
 
